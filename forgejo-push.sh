74840777d7bd8dbbdef75cb81117a343ec422c97
matthew lavelle <mattlavelle966@gmail.com>
2026-03-08 23:05:50 -0400

    improved validation, asks for user input

--- forgejo-push.sh
+++ forgejo-push.sh
@@ -2,112 +2,133 @@
 
 #set -e
 
-FORGEUSER="MattLavelle966"
+
+read -p "Enter Your username for the Forgejo: " FORGEUSER
+
 FORGEHOST="dropadox.sytes.net"
 FORGEPORT="2222"
 
+read -p "Enter in your Import target SSH-Key file (absolute): " SSH_KEY_IMPORT_REMOTE
+read -p "Enter in your Export target SSH-Key file (absolute): " SSH_KEY_EXPORT_REMOTE
+read -p "Enter the your JSON Array file path(absolute): " LISTPATH
+CURRENT_DIR=$(pwd)
+
+#result collectors 
 failed_repos=()
+succeded_repos=()
 
 
-if cd ~/repos/forgejo-automation/Sync/; then
+navToSyncDir(){
+  echo "Navigating to Sync dir"
+  if cd "$CURRENT_DIR/Sync/"; then
+    echo "Nav to Sync Dir has Successfull"
+    return 0
+  else
+    echo "Nav to Sync Dir has Failed"
+    return 1
+  fi
+}
+
+
+
+if navToSyncDir; then
   echo "Automation Init Complete"
 else
-  echo "Automation Init Failed"
-  exit 1
+  echo "Sync DIR not found attempting to make DIR.."
+  if mkdir -p "$CURRENT_DIR/Sync/"; then
+    if ! navToSyncDir; then
+      echo "Failed to create DIR quiting program"
+      exit 1
+    fi
+    echo "Sync Dir Created Automation Init Complete"
+  else
+    echo "Failed to create DIR quiting program"
+    exit 1
+  fi
 fi
 
-
+echo "--------------------------------------------------"
+#loading in repo list
 echo "starting Sync with $FORGEUSER"
-mapfile -t repositories < <(jq -r '.[]' ../repolist.json)
-
-
-
-
+mapfile -t repositories < <(jq -r '.[]' "$LISTPATH")
 for repo in "${repositories[@]}"; do
   echo "$repo"
 done
 echo "repo list loaded"
 
+echo "--------------------------------------------------"
+
+#main clone loop
 for repo in "${repositories[@]}"; do
-  ok=true
+  navToSyncDir
+  success=true
 
   repo_name="${repo##*/}"
-  echo "$repo"
+  repo_name="${repo_name%.git}"
 
+  echo "--------------------------------------------------"
   echo "Attempting clone of $repo"
-  if git clone "$repo"; then
+  if GIT_SSH_COMMAND="ssh -i \"${SSH_KEY_EXPORT_REMOTE}\" -o IdentitiesOnly=yes" \
+    git clone "$repo"; then
     echo "clone of $repo succeeded"
   else
-    ok=false
     echo "clone of $repo failed"
+    success=false
+    continue
+  fi
+
+  echo "Successfull clone of $repo"
+  echo "Navigating to repo"
+  
+  echo "--------------------------------------------------"
+  if cd "$repo_name"; then
+    echo "Nav to repo $repo_name succeeded"
+  else
+    echo "Nav to repo failed for $repo_name"
+    success=false
+    continue
+  fi
+
+  REPO_NAME=$(basename "$(git rev-parse --show-toplevel)")
+  REMOTE_URL="ssh://git@${FORGEHOST}:${FORGEPORT}/${FORGEUSER}/${REPO_NAME}.git"
+
+  if ! git remote | grep -q "^forgejo$"; then
+    git remote add forgejo "$REMOTE_URL"
+    echo "added remote: $REMOTE_URL"
+  else
+    echo "remote 'forgejo' already exists"
+
+  fi
+
+
+  echo "--------------------------------------------------"
+  echo "Pushing current branch: $(git branch --show-current)"
+  echo "Attempting a push of $repo to $REMOTE_URL"
+
+  if GIT_SSH_COMMAND="ssh -i \"${SSH_KEY_IMPORT_REMOTE}\" -p ${FORGEPORT} -o IdentitiesOnly=yes" \
+    git push --verbose -u forgejo HEAD; then
+    echo "Pushing $repo to $REMOTE_URL has succeeded"
+  else
+    echo "Pushing $repo to $REMOTE_URL has Failed"
+    success=false
+    continue
+  fi
+
+  navToSyncDir
+
+  if $success; then
+    succeded_repos+=("$repo")
+  else
     failed_repos+=("$repo")
   fi
-  
-  if $ok; then 
-    nav=true
-
-    echo "Successfull clone of $repo"
-    echo "Navigating to repo"
-    
-    if cd "$repo_name"; then
-      echo "Nav to repo $repo_name succeeded"
-    else
-      echo "Nav to repo failed for $repo_name"
-      nav=false
-      failed_repos+=("$repo")
-    fi
-
-    if $nav; then 
-      REPO_NAME=$(basename "$(git rev-parse --show-toplevel)")
-      REMOTE_URL="ssh://git@${FORGEHOST}:${FORGEPORT}/${FORGEUSER}/${REPO_NAME}.git"
-      success=true
-      
-
-      if ! git remote | grep -q "^forgejo$"; then
-        git remote add forgejo "$REMOTE_URL"
-        echo "added remote: $REMOTE_URL"
-      else
-        echo "remote 'forgejo' already exists"
-
-      fi
-
-      echo "Pushing current branch: $(git branch --show-current)"
-      
-      echo "Attempting a push of $repo to $REMOTE_URL"
-      if GIT_SSH_COMMAND="ssh -i ~/.ssh/forgejo_automation_key -p ${FORGEPORT} -o IdentitiesOnly=yes" \
-        git push --verbose -u forgejo HEAD; then
-        echo "Pushing $repo to $REMOTE_URL has succeeded"
-      else
-        echo "Pushing $repo to $REMOTE_URL has Failed"
-        failed_repos+=("$repo")
-        success=false
-      fi
-      
-
-      if $success; then
-        echo "Forgejo is now Synced to Github:$repo -> $REMOTE_URL"
-      else
-        echo "Failed to Sync Forgejo to Github:$repo -> $REMOTE_URL"
-      fi
-
-    fi
-
-    echo "returning to automation dir"
-    if cd ~/repos/forgejo-automation/Sync/; then
-      echo "Return to Sync Dir has Successfull"
-    else
-      echo "Return to Sync Dir has Failed"
-    fi
-  
-
-  else
-    echo "repo clone failed, skiping repo and moving onto next repo"
-  fi
-  
-  ok=true
+ 
 done
-
-echo "Failed Repos"
+echo "--------------------------------------------------"
+echo "Failed Repos:"
 for repo in "${failed_repos[@]}"; do
   echo "Repositorie Failed to Sync:{${repo}}"
 done
+
+
+
+
